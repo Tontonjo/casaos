a9fb0f77452fdddd5d063d253547678a9b096b66
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2023-05-19 11:42:48 +0200

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Tonton Jo - 2023
+# Version 1.0
+
+# Permit Root login:
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+# Install Casaos
+curl -fsSL https://get.casaos.io | sudo bash
