f1cad7e7f3f93b56d610c13fd76afb503e39da7f
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2023-05-19 12:22:25 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -6,8 +6,14 @@
 # Permit Root login:
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 
-# Install Casaos
-curl -fsSL https://get.casaos.io | bash
+# Setup casaos installation
+# Download startup script and make it executable
+wget -O /root/casaos_installation.sh https://github.com/Tontonjo/casaos/raw/main/casaos_installation.sh
+chmod +x /root/casaos_installation.sh
+
+# Add script to be executed on boot then removed once ran
+echo "#!/bin/sh -e \n/root/casaos_installation.sh \nrm -f /etc/rc.local \nexit 0" > /etc/rc.local
+chmod 755 /etc/rc.local
 
 # Install samba for hostname resolution
 apt update -y
